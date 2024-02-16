220ed1f74bce6a3ad7c09b946aae24dc5c23e781
Manojkumaranbuchezhian <160238295+Manojkumaranbuchezhian@users.noreply.github.com>
2024-02-16 20:24:33 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,144 +1,138 @@
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    image: "images/products/apple.jpg",
+    name: "apple seeds (10 Seeds)",
     rating: {
       stars: 4.5,
       count: 87
     },
-    priceCents: 1090,
+    priceCents: 200,
     keywords: [
-      "socks",
-      "sports",
-      "apparel"
+      "apple",
+      "kashmir",
+      "tree"
     ]
   },
   {
     id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
+    image: "images/products/banana.jpg",
+    name: "banana sapling",
     rating: {
       stars: 4,
       count: 127
     },
-    priceCents: 2095,
+    priceCents: 200,
     keywords: [
-      "sports",
-      "basketballs"
+      "banabna",
+      "tree",
+      "fruit"
     ]
   },
   {
     id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    image: "images/products/barley.jpg",
+    name: "barley seeds",
     rating: {
       stars: 4.5,
       count: 56
     },
     priceCents: 799,
     keywords: [
-      "tshirts",
-      "apparel",
-      "mens"
+      "barley",
+      "crop"
     ],
     type: "clothing",
     sizeChartLink: "images/clothing-size-chart.png"
   },
   {
     id: "54e0eccd-8f36-462b-b68a-8182611d9add",
-    image: "images/products/black-2-slot-toaster.jpg",
-    name: "2 Slot Toaster - Black",
+    image: "images/products/beans.jpg",
+    name: "beans seed",
     rating: {
       stars: 5,
       count: 2197
     },
     priceCents: 1899,
     keywords: [
-      "toaster",
-      "kitchen",
-      "appliances"
+      "bean",
+      "beans"
     ]
   },
   {
     id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
-    image: "images/products/6-piece-white-dinner-plate-set.jpg",
-    name: "6 Piece White Dinner Plate Set",
+    image: "images/products/blackberry.jpg",
+    name: "blackberry",
     rating: {
       stars: 4,
       count: 37
     },
     priceCents: 2067,
     keywords: [
-      "plates",
-      "kitchen",
-      "dining"
+      "blackberry",
+      "berry"
     ]
   },
   {
     id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
-    image: "images/products/6-piece-non-stick-baking-set.webp",
-    name: "6-Piece Nonstick, Carbon Steel Oven Bakeware Baking Set",
+    image: "images/products/bottle gourd.jpg",
+    name: "Battle gourd",
     rating: {
       stars: 4.5,
       count: 175
     },
     priceCents: 3499,
     keywords: [
-      "kitchen",
-      "cookware"
+      "bottle gourd",
+      "gourd"
     ]
   },
   {
     id: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
-    image: "images/products/plain-hooded-fleece-sweatshirt-yellow.jpg",
-    name: "Plain Hooded Fleece Sweatshirt",
+    image: "images/products/brinjal.jpg",
+    name: "Brinjal",
     rating: {
       stars: 4.5,
       count: 317
     },
     priceCents: 2400,
     keywords: [
-      "hoodies",
-      "sweaters",
-      "apparel"
+      "brinjal",
+      "plant"
     ]
   },
   {
     id: "77919bbe-0e56-475b-adde-4f24dfed3a04",
-    image: "images/products/luxury-tower-set-6-piece.jpg",
-    name: "Luxury Towel Set - Graphite Gray",
+    image: "images/products/cabbage.jpg",
+    name: "cabbage",
     rating: {
       stars: 4.5,
       count: 144
     },
     priceCents: 3599,
     keywords: [
-      "bathroom",
-      "washroom",
-      "restroom",
-      "towels",
-      "bath towels"
+      "leaf",
+      "cabbage"
     ]
   },
   {
     id: "3fdfe8d6-9a15-4979-b459-585b0d0545b9",
-    image: "images/products/liquid-laundry-detergent-plain.jpg",
-    name: "Liquid Laundry Detergent, 110 Loads, 82.5 Fl Oz",
+    image: "images/products/capsicum.jpg",
+    name: "Capsicum",
     rating: {
       stars: 4.5,
       count: 305
     },
     priceCents: 2899,
     keywords: [
-      "bathroom",
-      "cleaning"
+      "bell pepper",
+      "capsicum"
     ]
   },
   {
     id: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
-    image: "images/products/knit-athletic-sneakers-gray.jpg",
-    name: "Waterproof Knit Athletic Sneakers - Gray",
+    image: "images/products/carrot.jpg",
+    name: "carrot",
     rating: {
       stars: 4,
       count: 89
@@ -152,8 +146,8 @@ export const products = [
   },
   {
     id: "5968897c-4d27-4872-89f6-5bcb052746d7",
-    image: "images/products/women-chiffon-beachwear-coverup-black.jpg",
-    name: "Women's Chiffon Beachwear Cover Up - Black",
+    image: "images/products/cauliflower.jpg",
+    name: "cauliflower",
     rating: {
       stars: 4.5,
       count: 235
@@ -171,8 +165,8 @@ export const products = [
   },
   {
     id: "aad29d11-ea98-41ee-9285-b916638cac4a",
-    image: "images/products/round-sunglasses-black.jpg",
-    name: "Round Sunglasses",
+    image: "images/products/corn.jpg",
+    name: "corn",
     rating: {
       stars: 4.5,
       count: 30
@@ -185,8 +179,8 @@ export const products = [
   },
   {
     id: "04701903-bc79-49c6-bc11-1af7e3651358",
-    image: "images/products/women-beach-sandals.jpg",
-    name: "Women's Two Strap Buckle Sandals - Tan",
+    image: "images/products/cucumber.jpg",
+    name: "cucumber",
     rating: {
       stars: 4.5,
       count: 562
@@ -202,8 +196,8 @@ export const products = [
   },
   {
     id: "901eb2ca-386d-432e-82f0-6fb1ee7bf969",
-    image: "images/products/blackout-curtain-set-beige.webp",
-    name: "Blackout Curtains Set 4-Pack - Beige",
+    image: "images/products/custard apple.jpg",
+    name: "custard apple",
     rating: {
       stars: 4.5,
       count: 232
@@ -217,8 +211,8 @@ export const products = [
   },
   {
     id: "82bb68d7-ebc9-476a-989c-c78a40ee5cd9",
-    image: "images/products/men-slim-fit-summer-shorts-gray.jpg",
-    name: "Men's Slim-Fit Summer Shorts",
+    image: "images/products/dates.jpg",
+    name: "dates",
     rating: {
       stars: 4,
       count: 160
@@ -232,8 +226,8 @@ export const products = [
   },
   {
     id: "c2a82c5e-aff4-435f-9975-517cfaba2ece",
-    image: "images/products/electric-glass-and-steel-hot-water-kettle.webp",
-    name: "Electric Glass and Steel Hot Tea Water Kettle - 1.7-Liter",
+    image: "images/products/dargon fruit.jpg",
+    name: "dragon fruit",
     rating: {
       stars: 5,
       count: 846
@@ -247,8 +241,8 @@ export const products = [
   },
   {
     id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
-    image: "images/products/facial-tissue-2-ply-18-boxes.jpg",
-    name: "Ultra Soft Tissue 2-Ply - 18 Box",
+    image: "images/products/fig.jpg",
+    name: "fig",
     rating: {
       stars: 4,
       count: 99
@@ -264,8 +258,8 @@ export const products = [
   },
   {
     id: "a82c6bac-3067-4e68-a5ba-d827ac0be010",
-    image: "images/products/straw-sunhat.webp",
-    name: "Straw Lifeguard Sun Hat",
+    image: "images/products/garlic.jpg",
+    name: "garlic",
     rating: {
       stars: 4,
       count: 215
@@ -280,8 +274,8 @@ export const products = [
   },
   {
     id: "e4f64a65-1377-42bc-89a5-e572d19252e2",
-    image: "images/products/sky-flower-stud-earrings.webp",
-    name: "Sterling Silver Sky Flower Stud Earrings",
+    image: "images/products/grapes.jpg",
+    name: "grapes",
     rating: {
       stars: 4.5,
       count: 52
@@ -295,8 +289,8 @@ export const products = [
   },
   {
     id: "b0f17cc5-8b40-4ca5-9142-b61fe3d98c85",
-    image: "images/products/women-stretch-popover-hoodie-black.jpg",
-    name: "Women's Stretch Popover Hoodie",
+    image: "images/products/greenchilli.jpg",
+    name: "green chilli",
     rating: {
       stars: 4.5,
       count: 2465
@@ -314,8 +308,8 @@ export const products = [
   },
   {
     id: "a93a101d-79ef-4cf3-a6cf-6dbe532a1b4a",
-    image: "images/products/bathroom-rug.jpg",
-    name: "Bathroom Bath Rug Mat 20 x 31 Inch - Grey",
+    image: "images/products/ground nut.jpg",
+    name: "Ground nut",
     rating: {
       stars: 4.5,
       count: 119
@@ -329,8 +323,8 @@ export const products = [
   },
   {
     id: "4f4fbcc2-4e72-45cc-935c-9e13d79cc57f",
-    image: "images/products/women-knit-ballet-flat-black.jpg",
-    name: "Women's Knit Ballet Flat",
+    image: "images/products/guava.jpg",
+    name: "guava",
     rating: {
       stars: 4,
       count: 326
@@ -345,8 +339,8 @@ export const products = [
   },
   {
     id: "8b5a2ee1-6055-422a-a666-b34ba28b76d4",
-    image: "images/products/men-golf-polo-t-shirt-blue.jpg",
-    name: "Men's Regular-Fit Quick-Dry Golf Polo Shirt",
+    image: "images/products/ladies finger.jpg",
+    name: "ladies finger",
     rating: {
       stars: 4.5,
       count: 2556
@@ -363,8 +357,8 @@ export const products = [
   },
   {
     id: "b86ddc8b-3501-4b17-9889-a3bad6fb585f",
-    image: "images/products/trash-can-with-foot-pedal-50-liter.jpg",
-    name: "Trash Can with Foot Pedal - Brushed Stainless Steel",
+    image: "images/products/lemon.jpg",
+    name: "lemon",
     rating: {
       stars: 4.5,
       count: 2286
@@ -379,8 +373,8 @@ export const products = [
   },
   {
     id: "19c6a64a-5463-4d45-9af8-e41140a4100c",
-    image: "images/products/duvet-cover-set-blue-twin.jpg",
-    name: "Duvet Cover Set with Zipper Closure",
+    image: "images/products/mango.jpg",
+    name: "mango",
     rating: {
       stars: 4,
       count: 456
@@ -396,8 +390,8 @@ export const products = [
   },
   {
     id: "d2785924-743d-49b3-8f03-ec258e640503",
-    image: "images/products/women-chunky-beanie-gray.webp",
-    name: "Women's Chunky Cable Beanie - Gray",
+    image: "images/products/mint.jpg",
+    name: "mint",
     rating: {
       stars: 5,
       count: 83
@@ -414,8 +408,8 @@ export const products = [
   },
   {
     id: "ee1f7c56-f977-40a4-9642-12ba5072e2b0",
-    image: "images/products/men-chino-pants-beige.jpg",
-    name: "Men's Classic-fit Pleated Chino Pants",
+    image: "images/products/mulberry.jpg",
+    name: "mulberry",
     rating: {
       stars: 4.5,
       count: 9017
@@ -429,8 +423,8 @@ export const products = [
   },
   {
     id: "1c079479-8586-494f-ab53-219325432536",
-    image: "images/products/men-athletic-shoes-green.jpg",
-    name: "Men's Athletic Sneaker",
+    image: "images/products/onion.jpg",
+    name: "onion",
     rating: {
       stars: 4,
       count: 229
@@ -445,8 +439,8 @@ export const products = [
   },
   {
     id: "4df68c27-fd59-4a6a-bbd1-e754ddb6d53c",
-    image: "images/products/men-navigator-sunglasses-brown.jpg",
-    name: "Men's Navigator Sunglasses Pilot",
+    image: "images/products/papaya.jpg",
+    name: "papaya",
     rating: {
       stars: 3.5,
       count: 42
@@ -461,8 +455,8 @@ export const products = [
   },
   {
     id: "4e37dd03-3b23-4bc6-9ff8-44e112a92c64",
-    image: "images/products/non-stick-cooking-set-15-pieces.webp",
-    name: "Non-Stick Cookware Set, Pots, Pans and Utensils - 15 Pieces",
+    image: "images/products/pepper.jpg",
+    name: "Pepper",
     rating: {
       stars: 4.5,
       count: 511
@@ -475,8 +469,8 @@ export const products = [
   },
   {
     id: "a434b69f-1bc1-482d-9ce7-cd7f4a66ce8d",
-    image: "images/products/vanity-mirror-silver.jpg",
-    name: "Vanity Mirror with Heavy Base - Chrome",
+    image: "images/products/pineapple.jpg",
+    name: "pineapple",
     rating: {
       stars: 4.5,
       count: 130
@@ -491,8 +485,8 @@ export const products = [
   },
   {
     id: "a45cfa0a-66d6-4dc7-9475-e2b01595f7d7",
-    image: "images/products/women-french-terry-fleece-jogger-camo.jpg",
-    name: "Women's Fleece Jogger Sweatpant",
+    image: "images/products/plum.jpg",
+    name: "plum",
     rating: {
       stars: 4.5,
       count: 248
@@ -508,8 +502,8 @@ export const products = [
   },
   {
     id: "d339adf3-e004-4c20-a120-40e8874c66cb",
-    image: "images/products/double-elongated-twist-french-wire-earrings.webp",
-    name: "Double Oval Twist French Wire Earrings - Gold",
+    image: "images/products/pomegranate",
+    name: "pomegranate",
     rating: {
       stars: 4.5,
       count: 117
@@ -522,8 +516,8 @@ export const products = [
   },
   {
     id: "d37a651a-d501-483b-aae6-a9659b0757a0",
-    image: "images/products/round-airtight-food-storage-containers.jpg",
-    name: "Round Airtight Food Storage Containers - 5 Piece",
+    image: "images/products/potato.jpg",
+    name: "potato",
     rating: {
       stars: 4,
       count: 126
@@ -537,8 +531,8 @@ export const products = [
   },
   {
     id: "0d7f9afa-2efe-4fd9-b0fd-ba5663e0a524",
-    image: "images/products/coffeemaker-with-glass-carafe-black.jpg",
-    name: "Coffeemaker with Glass Carafe and Reusable Filter - 25 Oz, Black",
+    image: "images/products/pumpkin.jpg",
+    name: "pumpkin",
     rating: {
       stars: 4.5,
       count: 1211
@@ -552,8 +546,8 @@ export const products = [
   },
   {
     id: "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
-    image: "images/products/blackout-curtains-black.jpg",
-    name: "Blackout Curtains Set 42 x 84-Inch - Black, 2 Panels",
+    image: "images/products/radish.jpg",
+    name: "Radish",
     rating: {
       stars: 4.5,
       count: 363
@@ -566,8 +560,8 @@ export const products = [
   },
   {
     id: "8a53b080-6d40-4a65-ab26-b24ecf700bce",
-    image: "images/products/cotton-bath-towels-teal.webp",
-    name: "100% Cotton Bath Towels - 2 Pack, Light Teal",
+    image: "images/products/rice.jpg",
+    name: "rice",
     rating: {
       stars: 4.5,
       count: 93
@@ -581,8 +575,8 @@ export const products = [
   },
   {
     id: "10ed8504-57db-433c-b0a3-fc71a35c88a1",
-    image: "images/products/knit-athletic-sneakers-pink.webp",
-    name: "Waterproof Knit Athletic Sneakers - Pink",
+    image: "images/products/springonion",
+    name: "Spring onion",
     rating: {
       stars: 4,
       count: 89
@@ -597,8 +591,8 @@ export const products = [
   },
   {
     id: "77a845b1-16ed-4eac-bdf9-5b591882113d",
-    image: "images/products/countertop-blender-64-oz.jpg",
-    name: "Countertop Blender - 64oz, 1400 Watts",
+    image: "images/sugarcane.jpg",
+    name: "Sugarcane",
     rating: {
       stars: 4,
       count: 3
@@ -612,8 +606,8 @@ export const products = [
   },
   {
     id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
-    image: "images/products/floral-mixing-bowl-set.jpg",
-    name: "10-Piece Mixing Bowl Set with Lids - Floral",
+    image: "images/products/tomato.webp",
+    name: "Tomato",
     rating: {
       stars: 5,
       count: 679
@@ -628,8 +622,8 @@ export const products = [
   },
   {
     id: "aaa65ef3-8d6f-4eb3-bc9b-a6ea49047d8f",
-    image: "images/products/kitchen-paper-towels-30-pack.jpg",
-    name: "2-Ply Kitchen Paper Towels - 30 Pack",
+    image: "images/products/watermelon.jpg",
+    name: "watermelon",
     rating: {
       stars: 4.5,
       count: 1045
@@ -643,8 +637,8 @@ export const products = [
   },
   {
     id: "bc2847e9-5323-403f-b7cf-57fde044a955",
-    image: "images/products/men-cozy-fleece-zip-up-hoodie-red.jpg",
-    name: "Men's Full-Zip Hooded Fleece Sweatshirt",
+    image: "images/products/zuchinni.jpg",
+    name: "zuchinni",
     rating: {
       stars: 4.5,
       count: 3157
